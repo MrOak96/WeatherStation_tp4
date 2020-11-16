0dfbb8073a021b55281477e1e9de237176c25613
T3CHNOG4B <gabdeschesnes@gmail.com>
2020-11-16 15:28:21 -0500

    4 - OpenWeatherService

---
+++ WeatherApp/Services/OpenWeatherService.cs
@@ -0,0 +1,31 @@
+﻿using OpenWeatherAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+using WeatherApp.ViewModels;
+
+namespace WeatherApp.Services
+{
+    public class OpenWeatherService : ITemperatureService
+    {
+        private static OpenWeatherProcessor owp;
+
+        public TemperatureModel LastTemp = new TemperatureModel();
+
+        public OpenWeatherService(String apiKey)
+        {
+            owp = OpenWeatherProcessor.Instance;
+            owp.ApiKey = apiKey;
+        }
+
+        public async Task<TemperatureModel> GetTempAsync()
+        {
+            var current = await owp.GetCurrentWeatherAsync();
+            LastTemp.DateTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(current.DateTime).ToLocalTime();
+            LastTemp.Temperature = current.Main.Temperature;
+            return LastTemp;
+        }
+    }
+}
